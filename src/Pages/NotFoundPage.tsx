c39d5e494bea871c7e29aefe11d4ecb7eae93158
somaia96 <somaia96.sh@gmail.com>
2025-12-23 00:13:11 +0300

    Add A NotFound Page

--- src/Pages/NotFoundPage.tsx
+++ src/Pages/NotFoundPage.tsx
@@ -1,8 +1,18 @@
+import NotFoundSvg from "../assets/SVG/NotFoundSvg"
 const NotFoundPage = () => {
-    return (
-        <div>
-            NotFoundPage
+
+  return (
+    <div>
+      <a href="/">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 col-md-12 mt-5 mb-5">
+              <NotFoundSvg />
+            </div>
+          </div>
         </div>
-    )
+      </a>
+    </div>
+  )
 }
 export default NotFoundPage
