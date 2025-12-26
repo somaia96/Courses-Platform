b1608ec2ad072f3c47b8b401869d377f45cc1ae1
somaia96 <somaia96.sh@gmail.com>
2025-12-26 04:31:23 +0300

    Testimonials Section

--- src/component/Home/TestCard.tsx
+++ src/component/Home/TestCard.tsx
@@ -1,9 +1,24 @@
 import { ITestimonials } from "@/interfaces"
+import { Quote } from "lucide-react"
 
-const TestCard = ({client}:{client:ITestimonials}) => {
+const TestCard = ({ client }: { client: ITestimonials }) => {
     return (
-        <div>
-            {client.name}
+        <div className="text-gray-700 rounded-md bg-blue-100/10 shadow-lg p-5 pt-20 relative flex flex-col gap-5 justify-center">
+            <div className="w-20 h-20 flex justify-center items-center rounded-full overflow-hidden border-4 border-gray-300 shadow-lg absolute left-1/2 -translate-x-1/2 -top-5">
+                <img className="w-full" src={client.avatar} alt={client.name} />
+            </div>
+            <p className="relative text-sm">
+
+                <Quote className="inline-block mr-1" color="red" size={10} strokeWidth={1} />
+
+                {client.comment}
+
+                <Quote className="inline-block ml-1" color="red" size={10} strokeWidth={1} />
+
+            </p>
+            <p className="text-xs text-gray-800 font-medium">
+                {client.name} <span className="text-red-500">{client.role} in {client.company}</span>
+            </p>
         </div>
     )
 }
--- src/component/Home/Testimonials.tsx
+++ src/component/Home/Testimonials.tsx
@@ -3,8 +3,12 @@ import TestCard from "./TestCard"
 
 const Testimonials = () => {
     return (
-        <section className="py-20">
-            <div className="container grid grid-cols-1 md:grid-cols-3">
+        <section>
+            <h2 className="text-center text-3xl md:text-5xl font-medium text-gray-800 mb-3">Testimonials</h2>
+            <p className="text-center text-lg md:text-2xl text-gray-700 mb-20">
+                Our Client Testimonials Lorem ipsum dolor sit amet c
+            </p>
+            <div className="container grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
                 {testimonials.map(client=><TestCard key={client.id} client={client} />)}
             </div>
         </section>
